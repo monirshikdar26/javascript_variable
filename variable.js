bb86395a1d306d2d95495193418a802e7593e61d
monirshikdar26 <monirshikdar26@gmail.com>
2019-08-20 17:25:19 +0600

    javascript varible

---
+++ variable.js
@@ -0,0 +1,10 @@
+let name = 'monir',
+    age = 30,
+    massage = 'hello';
+alert(name);
+alert(age);
+alert(massage);
+/*
+    variable must contain letters, digits or the symbol $ and _
+    variable first character must not be a digit.
+*/
